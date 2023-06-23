5e61ea6914140f323559253d5e28c8d778a4012c
Muawiya Abubakar <muawiyaabk@gmail.com>
2023-06-23 15:22:37 -0700

    Add file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,12 +7,13 @@
 */
 int main(void)
 {
-	char ch = ('a', 'A');
+	char ch = 'a';
+	char ch1 = 'A';
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
-	for (ch = 'A'; ch <= 'Z'; ch++)
-		putchar(ch);
+	for (ch1 = 'A'; ch1 <= 'Z'; ch1++)
+		putchar(ch1);
 	putchar('\n');
 	return (0);
 }
