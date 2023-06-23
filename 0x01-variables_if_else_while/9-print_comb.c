07c74af31b5dd42a90567ca4723a98c8609a25ed
Muawiya Abubakar <muawiyaabk@gmail.com>
2023-06-23 16:43:41 -0700

    Add file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * author: Muawiya
+ * date:24-06-2023
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int a = 48;
+
+	for (a = 48; a < 58; a++)
+	{	
+		putchar(a);
+		if (a != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
