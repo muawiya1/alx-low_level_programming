fd274c1f9cd4e96d1eef3b2c6a5eeb515f32aa1a
Muawiya Abubakar <muawiyaabk@gmail.com>
2023-06-23 15:39:25 -0700

    Add print numbers file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * author: Muawiya
+ * date:23-06-2023
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int a = 0;
+
+	for (a = 1; a < 10; a++)
+		printf("%d", a);
+	return (0);
+}
