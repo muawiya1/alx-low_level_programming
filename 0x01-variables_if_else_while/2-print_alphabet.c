131c614dd9fe041aa8682eb9f93f43b61e219fe1
Muawiya Abubakar <muawiyaabk@gmail.com>
2023-06-23 14:58:26 -0700

    Add print_alphabet file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: Muawiya
+ * date:23-06-2023
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	for(ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+		putchar('\n');
+	return (0);
+}
