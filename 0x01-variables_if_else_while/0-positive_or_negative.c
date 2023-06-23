af44fe5fdda9e628f3e0fb3cf00aa80798265963
Muawiya Abubakar <muawiyaabk@gmail.com>
2023-06-23 09:00:58 -0700

    Adding correction file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,18 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+
+/**
+ * main -Entry point
+ * Author: Muawiya
+ * Date: 23-06-2023
+ * Details: print negative, zero and positive results of a random number using C.
+ * Return: 0 (success)
+ */
+
 int main(void)
 {
 	int n;
@@ -12,18 +21,18 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	/* your code goes there */
-	if (n>0)
-    	{
-        	printf("%d is positive\n",n);
-    	}
-    	else if (n==0)
-    	{
-        	printf("%d is zero\n",n);
-    	}
-    	else
-    	{
-        	printf("%d is negative\n",n);
-    	}
+	if (n > 0)
+    
+        printf("%d is positive\n", n);
+    
+    else if (n == 0)
+    
+        printf("%d is zero\n", n);
+    
+    else
+    
+        printf("%d is negative\n", n);
+    
 	return (0);
 }
 
