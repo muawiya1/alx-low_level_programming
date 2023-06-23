078f8e6b870680293e17fce5d77a55bb4bcd94b9
Muawiya Abubakar <muawiyaabk@gmail.com>
2023-06-23 15:58:45 -0700

    Add file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * author: Muawiya
+ * date:23-06-2023
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int a = 48;
+	
+	for (a = 48; a < 58; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
