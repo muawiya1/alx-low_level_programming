5fed7a391c4261b8c10c0c7e50ad164d2772144e
Muawiya Abubakar <muawiyaabk@gmail.com>
2023-06-23 02:02:02 -0700

    Add 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Author: Sammykingx
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	/*Printing a sentence*/
 
-	puts("\"Programming is like building a multilingual puzzle\"");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
