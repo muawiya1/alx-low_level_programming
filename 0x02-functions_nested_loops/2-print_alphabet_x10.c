d278390b1a45a803db4be19d9b996674f2445957
Muawiya Abubakar <muawiyaabk@gmail.com>
2023-06-30 12:52:18 -0700

    Add file 2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_alphabet - Entry point
+ * print_alphabet_x10 - Entry point
  * Decsription: Prints alphabet 10 times.
  * author: Muawiya
  * date:30-06-2023
  * Return: 0 (success)
 */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char c = 'a';
 	int i = 0;
