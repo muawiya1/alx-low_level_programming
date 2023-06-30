6efc7473c998d73cf8dc1d1fe5d43fbf1f336271
Muawiya Abubakar <muawiyaabk@gmail.com>
2023-06-30 15:44:15 -0700

    Add file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * Description: To check for lowercase.
+ * @c: Parameter to check and validate
+ * author: Muawiya
+ * date:23-06-2023
+ * Return: 0 (success)
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c >= 'Z'))
+		return (1);
+	else
+		return (0);
+}
