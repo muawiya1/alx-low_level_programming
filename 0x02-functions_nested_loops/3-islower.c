3b287e8fe721bb07edcb91fc9769a991a15abb1a
Muawiya Abubakar <muawiyaabk@gmail.com>
2023-06-30 15:32:57 -0700

    Add is lower file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * Description: To check for lowercase.
+ * @c: The parameter to check and verify
+ * author: Muawiya
+ * date:23-06-2023
+ * Return: 0 (success)
+*/
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'a')
+		return (1);
+	else
+		return (0);
+}
