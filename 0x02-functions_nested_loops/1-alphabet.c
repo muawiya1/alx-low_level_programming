ef2e6120ee4deb722b7e8974aca4e3ac61427cb5
Muawiya Abubakar <muawiyaabk@gmail.com>
2023-06-30 12:08:51 -0700

    Add 1-alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Description: C program to print_alphabet
+ * author: Muawiya
+ * date:23-06-2023
+ * Return: 0 (success)
+*/
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	return (0);
+}
