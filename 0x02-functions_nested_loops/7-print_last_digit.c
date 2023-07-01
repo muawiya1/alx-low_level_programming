57568320cb99f5cd3dffc8a2ad6929cc87449991
Muawiya Abubakar <muawiyaabk@gmail.com>
2023-07-01 12:06:51 -0700

    Add a file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * Description: To print last digit of a numbers
+ * @n: Parameter to check and validate
+ * author: Muawiya
+ * date:01-07-2023
+ * Return: 0 (success)
+*/
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
