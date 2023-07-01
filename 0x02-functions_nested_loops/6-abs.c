b450415b0472d4a7534617db49fd0d5cfd99e5ee
Muawiya Abubakar <muawiyaabk@gmail.com>
2023-07-01 12:01:29 -0700

    Add file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * Description: To return absoulte value
+ * @n: Parameter to check and validate
+ * author: Muawiya
+ * date:01-07-2023
+ * Return: 0 (success)
+*/
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
+
