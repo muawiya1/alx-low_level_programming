34d7974fc7efbda1d584e7cce840763e84c9a2eb
Muawiya Abubakar <muawiyaabk@gmail.com>
2023-07-04 05:52:40 -0700

    Add correction for 9-times_table file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,12 +24,12 @@ void times_table(void)
 				{
 					_putchar(c);
 				}
-				else if (b != 0)
+				if (b != 0)
 				{
 					_putchar(' ');
 					_putchar('0' + c);
 				}
-				else if (b < 9)
+				if (b < 9)
 				{
 					_putchar(',');
 					_putchar(' ');
