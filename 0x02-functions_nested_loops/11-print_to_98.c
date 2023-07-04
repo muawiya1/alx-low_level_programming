9d83722d7b971a947950de77c14935f419743d0f
Muawiya Abubakar <muawiyaabk@gmail.com>
2023-07-04 07:38:28 -0700

    Add file to print numbers to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Entry point
+ * Description: Print numbers up to 98
+ * @n: Starting point of counting
+ * author: Muawiya
+ * date:04-07-2023
+ * Return: 0 (success)
+*/
+
+void print_to_98(int n)
+{
+	int a;
+
+	for (n = a; n <= 98; n++)
+	{
+		printf("%d", n);
+
+		if (n < 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+
+	for (n = a; n >= 98; n--)
+	{
+		printf("%d", n);
+
+		if (n > 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
