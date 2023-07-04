0f4fb960ab7ecd7abd8604e70e2465e546ca3f27
Muawiya Abubakar <muawiyaabk@gmail.com>
2023-07-04 06:04:25 -0700

    Adding file for addition of two number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * Description: To add two numbers
+ * @a: Parameter to calculate
+ * @b: Parameter to calculate
+ * author: Muawiya
+ * date:04-07-2023
+ * Return: 0 (success)
+*/
+
+int add(int a, int b)
+{
+	int a, b;
+
+	n = a + b;
+
+	return (n);
+}
