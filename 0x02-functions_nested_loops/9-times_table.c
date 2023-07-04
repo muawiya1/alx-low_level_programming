c608118983bd590196398307dac450c472dfcb1c
Muawiya Abubakar <muawiyaabk@gmail.com>
2023-07-04 05:44:25 -0700

    Add 9-times_table file

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * times_table - Entry point
+ * Description: To print last all the times table to 9
+ * author: Muawiya
+ * date:04-07-2023
+ * Return: 0 (success)
+*/
+
+void times_table(void)
+{
+	int a, b;
+	int c = a * b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if (c < 10)
+			{
+				if (b == 0)
+				{
+					_putchar(c);
+				}
+				else if (b != 0)
+				{
+					_putchar(' ');
+					_putchar('0' + c);
+				}
+				else if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else if (c >= 10)
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
