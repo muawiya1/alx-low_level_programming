14a57d0f45406ccf432e8e1e5789154f2f287aec
Muawiya Abubakar <muawiyaabk@gmail.com>
2023-07-05 04:19:33 -0700

    Add file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -37,7 +37,7 @@ void print_times_table(int n)
 			} else
 			{
 				_putchar('0' + (c / 100));
-				_putchar('0' + ((c - 100) / 10);
+				_putchar('0' + ((c - 100) / 10));
 				_putchar('0' + (c % 10));
 			}
 			if (b < n)
